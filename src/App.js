b17d94e6e2d803d0d66fc823a360d899a3d6359f
Khefifi Salim <khefifisalim01@gmail.com>
2021-05-07 08:44:12 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import NavBar from "./Components/NavBar";
+import Welcome from "./Components/Welcome";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Welcome.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div>
+      <h1
+        style={{
+          color: "Red",
+          fontSize: "35px",
+          marginTop: "50px",
+          marginLeft: "400px",
+        }}
+      >
+        Welcome To My profile
+      </h1>
+    </div>
+  );
+};
+
+export default Welcome;
